247b151f7020f96188fb0462ebfa33090d687447
Raziel <raziel@codefresh.io>
2017-10-26 17:27:11 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,7 @@
 echo "parsing git commit info"
-echo $CF_COMMIT_MESSAGE
+echo "commit info = $CF_COMMIT_MESSAGE"
+export JIRA_ID=$(echo $GIT_COMMENT | grep -o "CF-\w*")
+export JIRA_URL=https://codefresh-io.atlassian.net/browse/$JIRA_ID
+echo "issue url = $JIRA_URL"
+echo JIRA_URL=$JIRA_URL
+echo JIRA_URL=$JIRA_URL >> $CF_VOLUME_PATH/env_vars_to_export
